1ae1a6ec35351074358eaf75d3b53d0d6f7eee77
kimminsu <xjfcnfw3@naver.com>
2023-03-30 23:15:17 +0900

    feat: Implement @LoginMember annotation

---
+++ backend/src/main/java/com/example/demo/security/LoginMemberArgumentResolver.java
@@ -0,0 +1,46 @@
+package com.example.demo.security;
+
+
+import com.example.demo.domain.member.Member;
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+
+    public static final String ANONYMOUS_USER = "anonymousUser";
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterType().isAssignableFrom(Member.class)
+            && parameter.hasParameterAnnotation(LoginMember.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+        NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            return getMember(authentication);
+        }
+        return null;
+    }
+
+    private Member getMember(Authentication authentication) {
+        if (isAnonymousUser(authentication)) {
+            return null;
+        }
+        MemberDetails principal = (MemberDetails) authentication.getPrincipal();
+        return principal.getMember();
+    }
+
+    private boolean isAnonymousUser(Authentication authentication) {
+        return authentication.getPrincipal().equals(ANONYMOUS_USER);
+    }
+}
--- backend/src/test/java/com/example/demo/controller/MemberControllerTest.java
+++ backend/src/test/java/com/example/demo/controller/MemberControllerTest.java
@@ -1,6 +1,9 @@
 package com.example.demo.controller;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.example.demo.common.WithCustomMember;
 import com.example.demo.config.WebConfig;
@@ -8,11 +11,16 @@ import com.example.demo.config.security.AuthProperties;
 import com.example.demo.config.security.WebSecurityConfig;
 import com.example.demo.security.AuthenticationTokenProvider;
 import com.example.demo.security.MemberDetailsService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @Import(WebSecurityConfig.class)
 @ComponentScan(basePackages = "com.example.demo.security")
@@ -28,10 +36,22 @@ class MemberControllerTest {
     @MockBean
     private MemberDetailsService service;
 
+    private MockMvc mvc;
+
+    @BeforeEach
+    void init(WebApplicationContext context) {
+        mvc = MockMvcBuilders
+            .webAppContextSetup(context)
+            .build();
+    }
+
     @Test
     @WithCustomMember
-    void test() {
-
+    void test() throws Exception {
+        mvc.perform(get("/api/members")
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andDo(print());
     }
 }
 
