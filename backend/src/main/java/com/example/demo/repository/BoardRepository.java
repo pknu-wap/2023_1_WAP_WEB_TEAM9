b8fd34273ee4245bcc4bc34902fdaaa48f7995f5
kimminsu <xjfcnfw3@naver.com>
2023-03-30 14:37:31 +0900

    feat: Implement Board Repository with Pessimistic lock

--- backend/src/main/java/com/example/demo/repository/BoardRepository.java
+++ backend/src/main/java/com/example/demo/repository/BoardRepository.java
@@ -1,8 +1,13 @@
 package com.example.demo.repository;
 
 import com.example.demo.domain.board.Board;
+import java.util.Optional;
+import javax.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
+    @Lock(value = LockModeType.PESSIMISTIC_WRITE)
+    Optional<Board> findBoardById(Long id);
 }
