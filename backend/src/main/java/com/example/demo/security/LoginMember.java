9be28481b3982a40d5e000fdcb8eb51afe0b9672
kimminsu <xjfcnfw3@naver.com>
2023-03-31 13:48:47 +0900

    refactor: Replace resolver with annotation

--- backend/src/main/java/com/example/demo/security/LoginMember.java
+++ backend/src/main/java/com/example/demo/security/LoginMember.java
@@ -4,9 +4,12 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
+@AuthenticationPrincipal(expression = "#this == 'anonymousUser' ? "
+    + "new com.example.demo.domain.member.Member() : Member")
 public @interface LoginMember {
 
 }
